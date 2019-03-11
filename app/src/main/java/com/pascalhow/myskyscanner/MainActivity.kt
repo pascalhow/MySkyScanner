9418957fb005f4e609e3e2e87f651603929b3452
pascalhow <pascal.how@mydrivesolutions.com>
2019-03-11 01:12:00 +0000

    First implementation of Flight Details screen

--- app/src/main/java/com/pascalhow/myskyscanner/MainActivity.kt
+++ app/src/main/java/com/pascalhow/myskyscanner/MainActivity.kt
@@ -8,6 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.root_layout, FlightDetailsFragment.newInstance(), FRAGMENT_FLIGHT_DETAILS_LIST)
+                .commit()
+        }
     }
-    
+
+    companion object {
+        const val FRAGMENT_FLIGHT_DETAILS_LIST = "fragment flight details list"
+    }
 }
---
+++ app/src/main/java/com/pascalhow/myskyscanner/model/FlightDetails.kt
@@ -0,0 +1,15 @@
+package com.pascalhow.myskyscanner.model
+
+data class FlightDetails(
+    var outboundTime:String = "15:35 - 17:00",
+    var outboundAirline: String = "EasyJet",
+    var outboundFlightType: String = "Direct",
+    var outboundFlightDuration: String = "2h25m",
+    var inboundTime:String = "15:35 - 17:00",
+    var inboundAirline: String = "EasyJet",
+    var inboundFlightType: String = "Direct",
+    var inboundFlightDuration: String = "2h25m",
+    var rating: String = "10",
+    var price: String = "£40",
+    var airlineUrl: String = "via easyJet.com"
+)
